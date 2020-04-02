7b74834abd48bcd52aeda7c76a749cb96e51df10
jadielmwangi <jedielmwangi@gmail.com>
2020-04-02 17:37:07 +0300

    adds bootsrap and link to styles.css to enhance responsiviness

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,63 @@
+// import { BrowserModule } from '@angular/platform-browser';
+// import { NgModule } from '@angular/core';
+
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+// import { QuoteCompComponent } from './quote-comp/quote-comp.component';
+// import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+// import { QuoteFormComponent } from './quote-form/quote-form.component';
+// import { HighlightQuoteDirective } from './highlight-quote.directive';
+// import { TimecountPipe } from './timecount.pipe';
+
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     QuoteCompComponent,
+//     QuoteDetailComponent,
+//     QuoteFormComponent,
+//     HighlightQuoteDirective,
+  
+//     TimecountPipe
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { QuoteCompComponent } from './quote-comp/quote-comp.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { TimeCountPipe } from './timecount.pipe';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
 import { HighlightQuoteDirective } from './highlight-quote.directive';
-// import { TimePipe } from './time.pipe';
-import { TimecountPipe } from './timecount.pipe';
-// import { HighlightQuotePipe } from './highlight-quote.pipe';
+// import { TimecountPipe } from './timecount.pipe';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteCompComponent,
     QuoteDetailComponent,
+    TimeCountPipe,
     QuoteFormComponent,
-    HighlightQuoteDirective,
-    // HighlightQuotePipe,
-    // TimePipe,
-    TimecountPipe
+    HighlightQuoteDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
